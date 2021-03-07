967fc583c51ff1f5d489dc298886ae4b9572fa5e
eugen.letterbox <eugen.letterbox@gmail.com>
2021-03-07 20:56:33 +0300

    - доделал основные функции у фронтенда

--- task-tracker-backend/app/Routes/index.js
+++ task-tracker-backend/app/Routes/index.js
@@ -4,3 +4,4 @@ const router = server.getRouter();
 router.post('/tasks/create', require('$Controllers/Tasks/Create'));
 router.post('/tasks/close', require('$Controllers/Tasks/Close'));
 router.post('/tasks/reopen', require('$Controllers/Tasks/Reopen'));
+router.get('/tasks/list', require('$Controllers/Tasks/List'));
